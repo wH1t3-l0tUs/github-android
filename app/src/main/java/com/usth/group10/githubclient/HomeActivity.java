e34b85537d1f21d29f7a6d160a0b39fd0c32edc2
nguyenduckhai <nguyenduckhai@gmail.com>
2018-10-27 20:22:24 +0700

    Add AndroidX

--- app/src/main/java/com/usth/group10/githubclient/HomeActivity.java
+++ app/src/main/java/com/usth/group10/githubclient/HomeActivity.java
@@ -1,13 +1,13 @@
 package com.usth.group10.githubclient;
 
-import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import com.google.android.material.navigation.NavigationView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
--- app/src/main/java/com/usth/group10/githubclient/ProfileActivity.java
+++ app/src/main/java/com/usth/group10/githubclient/ProfileActivity.java
@@ -1,12 +1,12 @@
 package com.usth.group10.githubclient;
 
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.PagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.tabs.TabLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ProfileActivity extends AppCompatActivity {
